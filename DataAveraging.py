3e1217cfcdc20316628fb7a1efb401cc994e5590
Otto Deng <ottodeng9@gmail.com>
2025-07-09 15:26:45 +0800

    Combines multiple variables into a single dataset
    Replaces single-variable data processing with logic to load,
    combine, and save multiple variables into one dataset for
    easier downstream analysis. Enhances data management and
    workflow efficiency by consolidating related variables into
    a unified file.

--- DataAveraging.py
+++ DataAveraging.py
@@ -1,59 +1,62 @@
 import numpy as np
 import xarray as xr
 
-var = input("请输入变量名（如'tcwv'）: ")
-# 加载数据
-filedirname = '/Users/ottodeng/Desktop/Fluctuation/ERA5SLP/data_stream-moda_stepType-avgua.nc'
-data = xr.open_dataset(filedirname)
-lat = data['latitude'].values
-lon = data['longitude'].values
+# 定义四个变量
+variables = ['tp', 'sst', 'tcwv', 'w']
 
-# 时间转换成年份
-AnnualMean = data.groupby('valid_time.year').mean(dim='valid_time')
-# cp = AnnualMean['cp']
-# tp = AnnualMean['tp']
+# 创建字典来存储所有变量的数据
+data_vars = {}
+coords = None
 
-var1 = AnnualMean[var]
+# 循环加载每个变量的文件
+for var in variables:
+    filename = f'/Users/ottodeng/Desktop/Fluctuation/ERA5SLP/mean_{var}.nc'
+    ds = xr.open_dataset(filename)
+    if var == 'w':
+        data_vars[var] = ds['vertical_SW']
+    else: 
+        data_vars[var] = ds[var]
+    
+    # 使用第一个文件的坐标系统
+    if coords is None:
+        coords = ds.coords
 
+# 创建新的组合Dataset
+combined_ds = xr.Dataset(data_vars, coords=coords)
 
-# 计算风速
-# WS = np.sqrt(np.array(UP)**2 + np.array(VP)**2)
+# 保存为新的.nc文件
+output_file = '/Users/ottodeng/Desktop/Fluctuation/ERA5SLP/combined_variables.nc'
+combined_ds.to_netcdf(output_file)
+print(f"组合数据已保存到 {output_file}")
 
-# 创建新的xarray.Dataset
-years = np.arange(1940, 2025)
-ds1 = xr.Dataset(
-    {
-        var: (['year', 'latitude', 'longitude'], var1.values),
-    },
-    coords={
-        'year': years,
-        'latitude': AnnualMean['latitude'],
-        'longitude': AnnualMean['longitude'],
-    }
-)
-
-
-# 保存为.nc文件
-output1 = f'/Users/ottodeng/Desktop/Fluctuation/ERA5SLP/mean_{var}.nc'
-ds1.to_netcdf(output1)
-print(f"数据已保存到 {output1}")
-
-# years = np.arange(1940, 2025)
-# ds2 = xr.Dataset(
-#     {
-#         'tp': (['year', 'latitude', 'longitude'], tp.values),
-#     },
-#     coords={
-#         'year': years,
-#         'latitude': AnnualMean['latitude'],
-#         'longitude': AnnualMean['longitude'],
-#     }
-# )
+# 关闭所有打开的数据集
+for var in variables:
+    filename = f'/Users/ottodeng/Desktop/Fluctuation/ERA5SLP/mean_{var}.nc'
+    ds = xr.open_dataset(filename)
+    ds.close()
 
 
 # # 保存为.nc文件
-# output2 = '/Users/ottodeng/Desktop/Fluctuation/ERA5SLP/mean_tp.nc'
-# ds2.to_netcdf(output2)
+# output1 = f'/Users/ottodeng/Desktop/Fluctuation/ERA5SLP/mean_{var}.nc'
+# ds1.to_netcdf(output1)
+# print(f"数据已保存到 {output1}")
+
+# # years = np.arange(1940, 2025)
+# # ds2 = xr.Dataset(
+# #     {
+# #         'tp': (['year', 'latitude', 'longitude'], tp.values),
+# #     },
+# #     coords={
+# #         'year': years,
+# #         'latitude': AnnualMean['latitude'],
+# #         'longitude': AnnualMean['longitude'],
+# #     }
+# # )
+
+
+# # # 保存为.nc文件
+# # output2 = '/Users/ottodeng/Desktop/Fluctuation/ERA5SLP/mean_tp.nc'
+# # ds2.to_netcdf(output2)
 
 
 
